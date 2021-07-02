03f10d819a6307ef545d5ab33a2d0705b58281de
Denis Grukhin <denis.grukhin@t-systems.com>
2021-07-02 19:14:40 +0300

    Initial commit

---
+++ src/main/java/denpear/javaee_rest_webapp/HelloApplication.java
@@ -0,0 +1,9 @@
+package denpear.javaee_rest_webapp;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
